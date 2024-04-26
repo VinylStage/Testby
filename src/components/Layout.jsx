9ecc9d871425b1b387488d1ea82eab170613dab8
VinylStage <mcpe9869@gmail.com>
2024-04-26 16:08:10 +0900

    Markdown 형식완료

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,15 +3,20 @@ import { Link } from 'gatsby-link'
 
 export default function Layout({ children }) {
     return (
-        <nav>
-            <div className='bg-blue-800 p-4 m-4 grid gird-cols-4'>
-                <Link to='/'><h1 className='my-2'>HOME</h1></Link>
-                <Link to='/blog'><h1 className='my-2'>BLOG</h1></Link>
-                <Link to='/posts'><h1 className='my-2'>POST</h1></Link>
-            </div>
-            <div>
-                {children}
-            </div>
-        </nav>
+        <>
+            <head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.css" />
+            </head>
+            <nav>
+                <div className='bg-blue-800 p-4 m-4 grid gird-cols-4'>
+                    <Link to='/'><h1 className='my-2'>HOME</h1></Link>
+                    <Link to='/blog'><h1 className='my-2'>BLOG</h1></Link>
+                    <Link to='/posts'><h1 className='my-2'>POST</h1></Link>
+                </div>
+                <div>
+                    {children}
+                </div>
+            </nav>
+        </>
     )
 }
